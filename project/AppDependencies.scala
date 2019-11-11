166fc222603aaa36724a152df4b700230b4d9d5d
Jenkins user <jenkins@ip-10-35-71-131.eu-west-2.compute.internal>
2019-11-11 15:53:15 +0000

    Creating new service transit-movements-trader-at-destination

---
+++ project/AppDependencies.scala
@@ -0,0 +1,22 @@
+import play.core.PlayVersion.current
+import play.sbt.PlayImport._
+import sbt.Keys.libraryDependencies
+import sbt._
+
+object AppDependencies {
+
+  val compile = Seq(
+
+    "uk.gov.hmrc"             %% "simple-reactivemongo"     % "7.20.0-play-26",
+    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "1.1.0"
+  )
+
+  val test = Seq(
+    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "1.1.0" % Test classifier "tests",
+    "org.scalatest"           %% "scalatest"                % "3.0.8"                 % "test",
+    "com.typesafe.play"       %% "play-test"                % current                 % "test",
+    "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test, it",
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "3.1.2"                 % "test, it"
+  )
+
+}
---
+++ project/plugins.sbt
@@ -0,0 +1,17 @@
+resolvers += Resolver.url("HMRC Sbt Plugin Releases", url("https://dl.bintray.com/hmrc/sbt-plugin-releases"))(
+  Resolver.ivyStylePatterns)
+
+resolvers += "HMRC Releases" at "https://dl.bintray.com/hmrc/releases"
+
+resolvers += Resolver.typesafeRepo("releases")
+
+
+addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "1.16.0")
+
+addSbtPlugin("uk.gov.hmrc" % "sbt-git-versioning" % "1.19.0")
+
+addSbtPlugin("uk.gov.hmrc" % "sbt-artifactory" % "0.19.0")
+
+addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "1.6.0")
+
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.6.20")
