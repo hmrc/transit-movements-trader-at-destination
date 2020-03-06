3adeb12416bdd9277d80f1c366390a9d7adb3cb2
ashwini.rudrappa <ashwini.rudrappa@digital.hmrc.gov.uk>
2020-03-06 11:31:36 +0000

    CTCTRADERS-297: add TestOnlyController to drop mongo collection

---
+++ app/controllers/testOnly/TestOnlyController.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.testOnly
+
+import javax.inject.Inject
+import play.api.i18n.I18nSupport
+import play.api.i18n.MessagesApi
+import play.api.mvc.Action
+import play.api.mvc.AnyContent
+import play.api.mvc.ControllerComponents
+import play.modules.reactivemongo.ReactiveMongoApi
+import reactivemongo.play.json.collection.JSONCollection
+import repositories.CollectionNames
+import uk.gov.hmrc.play.bootstrap.controller.BackendController
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+
+class TestOnlyController @Inject()(override val messagesApi: MessagesApi, mongo: ReactiveMongoApi, cc: ControllerComponents)(implicit ec: ExecutionContext)
+    extends BackendController(cc) {
+
+  def dropMongoCollection: Action[AnyContent] = Action.async {
+    implicit request =>
+      val collection: Future[JSONCollection] = mongo.database.map(_.collection[JSONCollection](CollectionNames.ArrivalMovementCollection))
+
+      collection.flatMap(
+        _.drop(failIfNotFound = false) map {
+          case true  => Ok(s"Dropped  '${CollectionNames.ArrivalMovementCollection}' Mongo collection")
+          case false => Ok("collection does not exist or something gone wrong")
+        }
+      )
+  }
+
+}
