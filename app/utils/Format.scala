c8841c195dc874967d5e45604dcfe47af79fb65f
Amaal Ali <9220299+amaalali@users.noreply.github.com>
2020-03-11 11:20:51 +0000

    Rename parameter names in util date and time formatters.

--- app/utils/Format.scala
+++ app/utils/Format.scala
@@ -24,13 +24,13 @@ import java.time.format.DateTimeFormatter
 
 object Format {
 
-  val dateFormatter: DateTimeFormatter           = DateTimeFormatter.ofPattern("yyyyMMdd")
-  def dateFormatted(date: LocalDate): String     = date.format(dateFormatter)
-  def dateFormatted(date: LocalDateTime): String = date.format(dateFormatter)
+  val dateFormatter: DateTimeFormatter               = DateTimeFormatter.ofPattern("yyyyMMdd")
+  def dateFormatted(date: LocalDate): String         = date.format(dateFormatter)
+  def dateFormatted(dateTime: LocalDateTime): String = dateTime.format(dateFormatter)
 
-  val timeFormatter: DateTimeFormatter           = DateTimeFormatter.ofPattern("HHmm")
-  def timeFormatted(date: LocalDateTime): String = date.format(timeFormatter)
-  def timeFormatted(date: LocalTime): String     = date.format(timeFormatter)
+  val timeFormatter: DateTimeFormatter               = DateTimeFormatter.ofPattern("HHmm")
+  def timeFormatted(dateTime: LocalDateTime): String = dateTime.format(timeFormatter)
+  def timeFormatted(time: LocalTime): String         = time.format(timeFormatter)
 
   def dateFormattedForHeader(dateTime: OffsetDateTime): String =
     dateTime.format(DateTimeFormatter.RFC_1123_DATE_TIME)
