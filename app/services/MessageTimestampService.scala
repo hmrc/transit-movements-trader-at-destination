0d2cf35ee6866ed2acb28f34acd63841ebf26700
Amaal Ali <9220299+amaalali@users.noreply.github.com>
2020-03-11 16:24:08 +0000

    Allow for an XML 007 message to be wrapped in a TimeStampedMessage.

---
+++ app/services/MessageTimestampService.scala
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package services
+
+import java.time.LocalDate
+import java.time.LocalTime
+
+import models.TimeStampedMessage
+
+import scala.concurrent.Future
+import scala.util.matching.Regex
+import scala.xml.Atom
+import scala.xml.NodeSeq
+
+class MessageTimestampService {
+
+  def deriveTimestamp(a: NodeSeq): Option[TimeStampedMessage[NodeSeq]] = {
+    val dateOfPrepRegex: Regex = """(\d{4})(\d{2})(\d{2})""".r.anchored
+    val timeOfPrepRegex: Regex = """(\d{2})(\d{2})""".r.anchored
+
+    val dateOfPrep: Option[LocalDate] =
+      (a \ "CC007A" \ "DatOfPreMES9").text match {
+        case dateOfPrepRegex(year, month, date) =>
+          Some(LocalDate.of(year.toInt, month.toInt, date.toInt)) // we shouldn't do unsafe casting like this but is it fine in this case because of the Regex?
+        case _ => None
+      }
+
+    val timeOfPrep: Option[LocalTime] =
+      (a \ "CC007A" \ "TimOfPreMES10").text match {
+        case timeOfPrepRegex(hours, minutes) =>
+          Some(LocalTime.of(hours.toInt, minutes.toInt)) // TODO: we shouldn't do unsafe casting like this but is it fine in this case because of the Regex?
+        case _ => None
+      }
+
+    /*
+      TODO: Make this applicative composition so that we can report back on ALL fields that are malformed. Also consider switching to using parsers.
+     */
+    for {
+      d <- dateOfPrep
+      t <- timeOfPrep
+    } yield TimeStampedMessage(d, t, a)
+  }
+}
---
+++ test/services/MessageTimestampServiceSpec.scala
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package services
+
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+
+import base.SpecBase
+import models.Message
+import models.TimeStampedMessage
+import org.scalatest.FunSuite
+import utils.Format
+
+class MessageTimestampServiceSpec extends SpecBase {
+
+  "makeMovement" - {
+    "return the message with the date and time of message preparation" in {
+      val application = app
+      val service     = application.injector.instanceOf[MessageTimestampService]
+
+      val dateOfPrep = LocalDate.now()
+      val timeOfPrep = LocalTime.of(1, 1)
+
+      val movement =
+        <transitRequest>
+          <CC007A>
+            <DatOfPreMES9>{Format.dateFormatted(dateOfPrep)}</DatOfPreMES9>
+            <TimOfPreMES10>{Format.timeFormatted(timeOfPrep)}</TimOfPreMES10>
+            <HEAHEA>
+              <DocNumHEA5>MRN</DocNumHEA5>
+            </HEAHEA>
+          </CC007A>
+        </transitRequest>
+
+      val expectedMessage = TimeStampedMessage(dateOfPrep, timeOfPrep, movement)
+
+      service.deriveTimestamp(movement).value mustEqual expectedMessage
+
+      app.stop()
+    }
+
+    "return a none when date of preparation is malformed" in {
+      val application = app
+      val service     = application.injector.instanceOf[MessageTimestampService]
+
+      val timeOfPrep = LocalTime.of(1, 1)
+
+      val movement =
+        <transitRequest>
+          <CC007A>
+            <DatOfPreMES9>198801211</DatOfPreMES9>
+            <TimOfPreMES10>{Format.timeFormatted(timeOfPrep)}</TimOfPreMES10>
+            <HEAHEA>
+              <DocNumHEA5>MRN</DocNumHEA5>
+            </HEAHEA>
+          </CC007A>
+        </transitRequest>
+
+      service.deriveTimestamp(movement) must not be (defined)
+
+      app.stop()
+    }
+
+    "return a none when time of preparation is malformed" in {
+      val application = app
+      val service     = application.injector.instanceOf[MessageTimestampService]
+
+      val dateOfPrep = LocalDate.now()
+
+      val movement =
+        <transitRequest>
+          <CC007A>
+            <DatOfPreMES9>{Format.dateFormatted(dateOfPrep)}</DatOfPreMES9>
+            <TimOfPreMES10>12311</TimOfPreMES10>
+            <HEAHEA>
+              <DocNumHEA5>MRN</DocNumHEA5>
+            </HEAHEA>
+          </CC007A>
+        </transitRequest>
+
+      service.deriveTimestamp(movement) must not be (defined)
+
+      app.stop()
+    }
+
+  }
+
+}
