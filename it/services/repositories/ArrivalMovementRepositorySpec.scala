fcce078483364780d20af5c8709ae5a8bf1bb045
Matthew Beck <dougledog@live.co.uk>
2020-03-13 15:54:05 +0000

    CTCTRADERS-324: Minor refactor

--- it/services/repositories/ArrivalMovementRepositorySpec.scala
+++ it/services/repositories/ArrivalMovementRepositorySpec.scala
@@ -20,12 +20,20 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 class ArrivalMovementRepositorySpec extends FreeSpec with MustMatchers with MongoSuite with FailOnUnindexedQueries with ScalaFutures with GuiceOneAppPerSuite with IntegrationPatience with MockDateTimeService with OptionValues with ScalaCheckPropertyChecks with MessageGenerators {
 
+  private val eoriNumber: String = arbitrary[String].sample.value
+  private val arrivalMovement1: ArrivalMovement = arbitrary[ArrivalMovement].sample.value.ensuring(_.eoriNumber != eoriNumber)
+  private val arrivalMovement2: ArrivalMovement = arbitrary[ArrivalMovement].sample.value.ensuring(_.eoriNumber != eoriNumber)
 
   private lazy val builder = new GuiceApplicationBuilder()
 
+  override protected def afterEach(): Unit = {
+    super.afterEach
+    database.flatMap(_.drop()).futureValue
+  }
+
   "ArrivalMovementRepository" - {
     "must persist ArrivalMovement within mongoDB" in {
-      val arrivalMovement = arbitrary[ArrivalMovement].sample.value
+
       val app: Application = builder.build()
 
       running(app) {
@@ -33,15 +41,15 @@ class ArrivalMovementRepositorySpec extends FreeSpec with MustMatchers with Mong
 
         val service: ArrivalMovementRepository = app.injector.instanceOf[ArrivalMovementRepository]
 
-        service.persistToMongo(arrivalMovement).futureValue
+        service.persistToMongo(arrivalMovement1).futureValue
 
-        val selector = Json.obj("eoriNumber" -> arrivalMovement.eoriNumber)
+        val selector = Json.obj("eoriNumber" -> arrivalMovement1.eoriNumber)
 
         val getValue: Option[ArrivalMovement] = database.flatMap { result =>
           result.collection[JSONCollection](CollectionNames.ArrivalMovementCollection).find(selector, None).one[ArrivalMovement]
         }.futureValue
 
-        getValue.value mustBe arrivalMovement
+        getValue.value mustBe arrivalMovement1
       }
     }
 
@@ -49,15 +57,12 @@ class ArrivalMovementRepositorySpec extends FreeSpec with MustMatchers with Mong
       "must fetch all the ArrivalMovements from mongoDB and filter non-matching eori's" in {
         val app: Application = builder.build()
 
-        val arrivalMovement = arbitrary[ArrivalMovement].sample.value
-        val arrivalMovementWithDifferentEori = arbitrary[ArrivalMovement].sample.value
-
         running(app) {
           started(app).futureValue
 
           val service: ArrivalMovementRepository = app.injector.instanceOf[ArrivalMovementRepository]
 
-          val allMovements = Seq(arrivalMovement, arrivalMovementWithDifferentEori)
+          val allMovements = Seq(arrivalMovement1, arrivalMovement2)
 
           val jsonArr = allMovements.map(Json.toJsObject(_))
 
@@ -65,19 +70,14 @@ class ArrivalMovementRepositorySpec extends FreeSpec with MustMatchers with Mong
             db.collection[JSONCollection](CollectionNames.ArrivalMovementCollection).insert(false).many(jsonArr)
           }.futureValue
 
-          val result: Seq[ArrivalMovement] = service.fetchAllMovements(arrivalMovement.eoriNumber).futureValue
+          val result: Seq[ArrivalMovement] = service.fetchAllMovements(arrivalMovement1.eoriNumber).futureValue
 
-          result mustBe Seq(arrivalMovement)
+          result mustBe Seq(arrivalMovement1)
         }
       }
 
       "must return an empty sequence when there are no movements with the same eori" in {
         val app: Application = builder.build()
-
-        val arrivalMovement1 = arbitrary[ArrivalMovement].sample.value
-        val arrivalMovement2 = arbitrary[ArrivalMovement].sample.value
-
-        val eoriNumber = arbitrary[String].sample.value
 
         running(app) {
           started(app).futureValue
