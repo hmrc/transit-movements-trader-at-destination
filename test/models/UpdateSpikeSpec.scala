6bf521e36d4d3cc4c08ef401d665e0ca5d4010fc
Amaal Ali <9220299+amaalali@users.noreply.github.com>
2020-06-08 13:14:34 +0100

    Add semigroup tests.

--- test/models/UpdateSpikeSpec.scala
+++ test/models/UpdateSpikeSpec.scala
@@ -16,12 +16,19 @@
 
 package models
 
+import base.FreeSpecDiscipline
 import cats._
+import cats.kernel.laws.discipline.SemigroupTests
+import generators.ModelGenerators
+import org.scalacheck.Arbitrary
+import org.scalacheck.Gen
+import org.scalacheck.Arbitrary.arbitrary
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
+import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 import play.api.libs.json.Json
 
-class UpdateSpikeSpec extends AnyFreeSpec with Matchers {
+class UpdateSpikeSpec extends AnyFreeSpec with Matchers with ScalaCheckDrivenPropertyChecks with FreeSpecDiscipline with ModelGenerators {
 
   val aStatusUpdate  = AStatusUpdate(ArrivalStatus.Initialized)
   val aStatusUpdate2 = AStatusUpdate(ArrivalStatus.ArrivalSubmitted)
@@ -31,6 +38,29 @@ class UpdateSpikeSpec extends AnyFreeSpec with Matchers {
   val mStatusUpdate3 = MStatusUpdate(MessageId.fromIndex(1), MessageStatus.SubmissionSucceeded)
   val mStatusUpdate4 = MStatusUpdate(MessageId.fromIndex(2), MessageStatus.SubmissionSucceeded)
 
+  implicit val eqUpdateSpike: Eq[UpdateSpike] =
+    new Eq[UpdateSpike] {
+      override def eqv(x: UpdateSpike, y: UpdateSpike): Boolean = x == y
+    }
+
+  implicit val arbitraryAStatusUpdate: Arbitrary[AStatusUpdate] = Arbitrary(arbitrary[ArrivalStatus].map(AStatusUpdate(_)))
+  implicit val arbitraryMStatusUpdate: Arbitrary[MStatusUpdate] = Arbitrary {
+    for {
+      messageId     <- arbitrary[MessageId]
+      messageStatus <- arbitrary[MessageStatus]
+    } yield MStatusUpdate(messageId, messageStatus)
+  }
+  implicit val arbitraryApMStatusUpdate: Arbitrary[ApMStatusUpdate] = Arbitrary {
+    for {
+      arrivalStatus <- arbitrary[ArrivalStatus]
+      messageId     <- arbitrary[MessageId]
+      messageStatus <- arbitrary[MessageStatus]
+    } yield ApMStatusUpdate(arrivalStatus, messageId, messageStatus)
+  }
+
+  implicit val arbitraryUpdateSpike: Arbitrary[UpdateSpike] = Arbitrary(
+    Gen.oneOf(arbitrary[AStatusUpdate], arbitrary[MStatusUpdate], arbitrary[ApMStatusUpdate]))
+
   "aStatusUpdate" - {
     "aStatusUpdate + aStatusUpdate = rhs" in {
       Semigroup.combine[UpdateSpike](aStatusUpdate, aStatusUpdate2) mustEqual aStatusUpdate2
@@ -38,6 +68,9 @@ class UpdateSpikeSpec extends AnyFreeSpec with Matchers {
   }
 
   "ArrivalModifier" - {
+
+    checkAll("Semigroup behaviour", SemigroupTests[UpdateSpike].semigroup)
+
     "asdf" in {
 
       val expectedJson = Json.obj(
