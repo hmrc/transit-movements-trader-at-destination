933cbf51229a5b29bb7fc5b4aad77111cf6ee9c5
Matthew Beck <matthew.beck@digital.hmrc.gov.uk>
2020-08-04 09:16:07 +0100

    CTCTRADERS-718: Resolved conflicts

--- test/controllers/PDFGenerationControllerSpec.scala
+++ test/controllers/PDFGenerationControllerSpec.scala
@@ -30,6 +30,7 @@ import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 import play.api.inject.bind
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
+import repositories.ArrivalMovementRepository
 import services.UnloadingPermissionPDFService
 
 import scala.concurrent.Future
@@ -51,10 +52,12 @@ class PDFGenerationControllerSpec extends SpecBase with ScalaCheckPropertyChecks
     "getPDF" - {
 
       "must return OK and a PDF" in {
-        when(mockUnloadingPermissionPDFService.getPDF(any())(any(), any())).thenReturn(Future.successful(Some(HttpResponse(200, ""))))
 
-        forAll(genArrivalWithSuccessfulArrival) {
-          arrival =>
+        forAll(genArrivalWithSuccessfulArrival, arbitrary[Array[Byte]]) {
+          (arrival, pdf) =>
+            when(mockUnloadingPermissionPDFService.getPDF(any())(any(), any()))
+              .thenReturn(Future.successful(Right(pdf)))
+
             val fakeGerArrivalReader = FakeAuthenticatedGetArrivalForReadActionProvider(arrival)
             val application = baseApplicationBuilder
               .overrides(
@@ -73,12 +76,12 @@ class PDFGenerationControllerSpec extends SpecBase with ScalaCheckPropertyChecks
       }
 
       "must return NotFound when unloading permission is missing" in {
-        when(mockUnloadingPermissionPDFService.getPDF(any())(any(), any())).thenReturn(Future.successful(None))
         forAll(genArrivalWithSuccessfulArrival) {
           arrival =>
+            when(mockUnloadingPermissionPDFService.getPDF(any())(any(), any()))
+              .thenReturn(Future.successful(Left(NotFoundError)))
+
             val fakeGerArrivalReader = FakeAuthenticatedGetArrivalForReadActionProvider(arrival)
-            when(mockArrivalMovementRepository.get(any())).thenReturn(Future.successful(Some(arrival)))
-            when(mockUnloadingPermissionPDFService.getPDF(any())(any(), any())).thenReturn(Future.successful(Left(NotFoundError)))
 
             val application = baseApplicationBuilder
               .overrides(
@@ -96,34 +99,15 @@ class PDFGenerationControllerSpec extends SpecBase with ScalaCheckPropertyChecks
         }
       }
 
-      "must return BadGateway when there is a error in the other microservice" in {
-
-        when(mockUnloadingPermissionPDFService.getPDF(any())(any(), any())).thenReturn(Future.successful(Some(HttpResponse(500, ""))))
-        forAll(genArrivalWithSuccessfulArrival) {
-          arrival =>
-            val fakeGerArrivalReader = FakeAuthenticatedGetArrivalForReadActionProvider(arrival)
-            val application = baseApplicationBuilder
-              .overrides(
-                bind[AuthenticatedGetArrivalForReadActionProvider].toInstance(fakeGerArrivalReader),
-                bind[UnloadingPermissionPDFService].toInstance(mockUnloadingPermissionPDFService)
-              )
-              .build()
-
-            running(application) {
-              val request = FakeRequest(GET, routes.PDFGenerationController.getPDF(arrival.arrivalId).url)
-              val result  = route(application, request).value
-
-              status(result) mustEqual BAD_GATEWAY
-            }
-        }
-      }
-
-      "must return InternalServerError when there is any other errors" in {
+      "must return BadGateway when there is any other errors" in {
         val genErrorCode = Gen.oneOf(300, 499)
 
         forAll(genArrivalWithSuccessfulArrival, genErrorCode) {
           (arrival, errorCode) =>
-            when(mockUnloadingPermissionPDFService.getPDF(any())(any(), any())).thenReturn(Future.successful(Some(HttpResponse(errorCode, ""))))
+            when(mockArrivalMovementRepository.get(any())).thenReturn(Future.successful(Some(arrival)))
+            when(mockUnloadingPermissionPDFService.getPDF(any())(any(), any()))
+              .thenReturn(Future.successful(Left(OtherError(errorCode, ""))))
+
             val fakeGerArrivalReader = FakeAuthenticatedGetArrivalForReadActionProvider(arrival)
 
             val application = baseApplicationBuilder
