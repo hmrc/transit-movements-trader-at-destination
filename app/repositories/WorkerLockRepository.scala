115476aa75ee764a3eae34553d5a6a15b5141365
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2021-01-15 15:25:18 +0000

    Add worker lock repository

---
+++ app/repositories/WorkerLockRepository.scala
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package repositories
+
+import java.time.LocalDateTime
+
+import config.AppConfig
+import javax.inject.Inject
+import play.api.libs.json.Json
+import play.modules.reactivemongo.ReactiveMongoApi
+import reactivemongo.api.commands.LastError
+import reactivemongo.api.indexes.Index.Aux
+import reactivemongo.api.indexes.IndexType
+import reactivemongo.bson.BSONDocument
+import reactivemongo.play.json.ImplicitBSONHandlers.JsObjectDocumentWriter
+import reactivemongo.play.json.collection.JSONCollection
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+import models.MongoDateTimeFormats._
+import reactivemongo.api.bson.collection.BSONSerializationPack
+import utils.IndexUtils
+
+class WorkerLockRepository @Inject()(mongo: ReactiveMongoApi, appConfig: AppConfig)(implicit ec: ExecutionContext) {
+
+  private val documentExistsErrorCodeValue = 11000
+
+  private val ttl = appConfig.lockRepositoryTtl
+
+  private def collection: Future[JSONCollection] =
+    mongo.database.map(_.collection[JSONCollection](WorkerLockRepository.collectionName))
+
+  private val createdIndex: Aux[BSONSerializationPack.type] = IndexUtils.index(
+    key = Seq("created" -> IndexType.Ascending),
+    name = Some("created-index"),
+    options = BSONDocument("expireAfterSeconds" -> ttl)
+  )
+
+  val started: Future[Boolean] =
+    collection
+      .flatMap {
+        _.indexesManager.ensure(createdIndex)
+      }
+      .map(_ => true)
+
+  def lock(id: String): Future[Boolean] = {
+
+    val lock = Json.obj(
+      "_id"     -> id,
+      "created" -> LocalDateTime.now
+    )
+
+    collection.flatMap {
+      _.insert(ordered = false)
+        .one(lock)
+        .map(_ => true)
+    } recover {
+      case e: LastError if e.code.contains(documentExistsErrorCodeValue) =>
+        false
+    }
+  }
+
+  def unlock(id: String): Future[Unit] =
+    collection.flatMap {
+      _.findAndRemove(Json.obj("_id" -> id))
+        .map(_ => ())
+    }
+}
+
+object WorkerLockRepository {
+
+  val collectionName = "worker-locks"
+}
---
+++ it/repositories/WorkerLockRepositorySpec.scala
@@ -0,0 +1,150 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package repositories
+
+import base.ItSpecBase
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.libs.json.JsObject
+import play.api.libs.json.JsSuccess
+import play.api.libs.json.Json
+import play.api.test.Helpers._
+import reactivemongo.play.json.ImplicitBSONHandlers.JsObjectDocumentWriter
+import reactivemongo.play.json.collection.JSONCollection
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class WorkerLockRepositorySpec extends ItSpecBase with FailOnUnindexedQueries with ScalaCheckPropertyChecks {
+
+  "lock" - {
+    "must lock an id when it is not already locked" in {
+      database.flatMap(_.drop()).futureValue
+
+      val app = new GuiceApplicationBuilder().build()
+
+      val id = "foo"
+
+      running(app) {
+        started(app).futureValue
+
+        val repository = app.injector.instanceOf[WorkerLockRepository]
+
+        val result = repository.lock(id).futureValue
+
+        result mustEqual true
+
+        val selector = Json.obj("_id" -> id)
+        val lock = database.flatMap {
+          db =>
+            db.collection[JSONCollection](WorkerLockRepository.collectionName).find(selector, None).one[JsObject]
+        }.futureValue
+
+        lock.value("_id").validate[String] mustEqual JsSuccess(id)
+      }
+    }
+
+    "must not lock an id that is already locked" in {
+      database.flatMap(_.drop()).futureValue
+
+      val app = new GuiceApplicationBuilder().build()
+
+      val id = "foo"
+
+      running(app) {
+        started(app).futureValue
+
+        val repository = app.injector.instanceOf[WorkerLockRepository]
+
+        val result1 = repository.lock(id).futureValue
+        val result2 = repository.lock(id).futureValue
+
+        result1 mustEqual true
+        result2 mustEqual false
+      }
+    }
+  }
+
+  "unlock" - {
+    "must remove an existing lock" in {
+      database.flatMap(_.drop()).futureValue
+
+      val app = new GuiceApplicationBuilder().build()
+
+      val id = "foo"
+
+      running(app) {
+        started(app).futureValue
+
+        val repository = app.injector.instanceOf[WorkerLockRepository]
+
+        repository.lock(id).futureValue
+        repository.unlock(id).futureValue
+
+        val selector = Json.obj("_id" -> id)
+        val remainingLock = database.flatMap {
+          db =>
+            db.collection[JSONCollection](WorkerLockRepository.collectionName).find(selector, None).one[JsObject]
+        }.futureValue
+
+        remainingLock must not be defined
+      }
+    }
+
+    "must not fail when asked to remove a lock that doesn't exist" in {
+      database.flatMap(_.drop()).futureValue
+
+      val app = new GuiceApplicationBuilder().build()
+
+      val id = "foo"
+
+      running(app) {
+        started(app).futureValue
+
+        val repository = app.injector.instanceOf[WorkerLockRepository]
+
+        repository.unlock(id).futureValue
+      }
+    }
+  }
+
+  "BSON formatting for ttl index" - {
+    "a lock's created field must be a date for the ttl index" in {
+      database.flatMap(_.drop()).futureValue
+
+      val app = new GuiceApplicationBuilder().build()
+
+      val id = "foo"
+
+      running(app) {
+        started(app).futureValue
+
+        val repository = app.injector.instanceOf[WorkerLockRepository]
+
+        val result = repository.lock(id).futureValue
+
+        result mustEqual true
+
+        val selector = Json.obj("_id" -> id, "created" -> Json.obj("$type" -> "date"))
+
+        val lock = database.flatMap(_.collection[JSONCollection](WorkerLockRepository.collectionName).find(selector, None).one[JsObject]).futureValue
+
+        lock must be(defined)
+      }
+
+    }
+  }
+}
