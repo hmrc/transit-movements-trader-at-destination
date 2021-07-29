3d257cb15143947920c2ec2cd704dff1a4929d91
David Gregory <DavidGregory084@users.noreply.github.com>
2021-07-29 10:48:13 +0100

    Fix conflict in ArrivalMessageNotificationSpec

--- test/models/ArrivalMessageNotificationSpec.scala
+++ test/models/ArrivalMessageNotificationSpec.scala
@@ -58,6 +58,7 @@ class ArrivalMessageNotificationSpec extends SpecBase with ScalaCheckDrivenPrope
         ArrivalMessageNotification(
           s"/customs/transits/movements/arrivals/${arrival.arrivalId.index}/messages/${arrival.messages.length + 1}",
           s"/customs/transits/movements/arrivals/${arrival.arrivalId.index}",
+          arrival.eoriNumber,
           arrival.arrivalId,
           MessageId.fromIndex(arrival.messages.length),
           now,
